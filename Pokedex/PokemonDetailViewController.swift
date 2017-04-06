f293c5fa4f75f45c0007d08c44fce0728e485b3e
Douglas Heitner <douglas.heitner@gmail.com>
2017-04-06 14:41:50 -0700

    Finished details UI.

--- Pokedex/PokemonDetailViewController.swift
+++ Pokedex/PokemonDetailViewController.swift
@@ -11,8 +11,19 @@ import UIKit
 class PokemonDetailViewController: UIViewController {
     
     @IBOutlet weak var nameLbl: UILabel!
-
-
+    @IBOutlet weak var mainImage: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var evoLbl: UILabel!
+    @IBOutlet weak var currentEvoImage: UIImageView!
+    @IBOutlet weak var nextEvoImage: UIImageView!
+    
+    
     var pokemon: Pokemon!
     
     override func viewDidLoad() {
